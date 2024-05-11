dc3bec3c7c5b38be2a698dbadb7c84b79ca85b71
Marcello Formica <formicalab@live.it>
2024-05-11 12:39:27 +0200

    fix update.sh to use old scp protocol

--- applications/luci-app-asterisk-status/update.sh
+++ applications/luci-app-asterisk-status/update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Update the Asterisk CEL module
-scp -r root/* root@192.168.1.1:/
-scp -r htdocs/* root@192.168.1.1:/www/
+scp -O -r root/* root@192.168.1.1:/
+scp -O -r htdocs/* root@192.168.1.1:/www/
